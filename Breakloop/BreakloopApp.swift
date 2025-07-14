9d392e56d1640b69a70a5b5bc587f041f582573c
Asaf <xxtrdnry@gmail.com>
2025-07-14 13:22:44 -0700

    Initial Commit

---
+++ Breakloop/BreakloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakloopApp.swift
+//  Breakloop
+//
+//  Created by ASAF GOREN on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BreakloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakloopTests/BreakloopTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreakloopTests.swift
+//  BreakloopTests
+//
+//  Created by ASAF GOREN on 7/14/25.
+//
+
+import Testing
+@testable import Breakloop
+
+struct BreakloopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
